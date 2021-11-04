e30882086807b42ae6bfbfee8af34011accd3e85
Lindson França <l1nds0n@hotmail.com>
2021-11-04 14:50:17 -0300

    routes and contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
-
 import { BrowserRouter, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom'; 
 
 function App() {
+
   return (
     <div>
-      {/* <Home /> */}
-      <NewRoom />
       <BrowserRouter>
-        <Route />
+        <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+        </AuthContextProvider>
       </BrowserRouter>
     </div>
   );
