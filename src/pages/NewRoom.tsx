92461ebbba8c5c51f10b7d0fcab2f3847f7c4099
Lindson França <l1nds0n@hotmail.com>
2021-11-04 14:49:37 -0300

    navigation and auth added

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
-
-import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { useAuth } from '../contexts';
+import '../styles/auth.scss';
+
+
+
 export function NewRoom() {
+  const { user } = useAuth();
+
   return (
     <div id="page-auth">
       <aside>
@@ -25,7 +30,9 @@ export function NewRoom() {
             <input type="text" placeholder="Nome da sala" />
             <Button type="submit">Criar sala</Button>
           </form>
-          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
