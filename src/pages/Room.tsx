b01f800e32bb0eb6486cd5b0d0b2ecb81469f898
Lindson França <l1nds0n@hotmail.com>
2021-11-08 11:16:09 -0300

    room's improvement

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
@@ -8,15 +8,65 @@ import { useAuth } from '../contexts';
 import { database } from '../services/firebase';
 import '../styles/room.scss';
 
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+};
+
 type RoomParams = {
   id: string;
 };
 
 export function Room() {
   const { user } = useAuth();
+  const [newQuestion, setNewQuestion] = useState('');
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [title, setTitle] = useState('');
   const params = useParams<RoomParams>();
   const roomId = params.id;
-  const [newQuestion, setNewQuestion] = useState('');
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isAnswered: value.isAnswered,
+            isHighlighted: value.isHighlighted,
+          };
+        },
+      );
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
@@ -51,8 +101,10 @@ export function Room() {
       </header>
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>
+            Sala <strong>{title}</strong>
+          </h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
         <form onSubmit={handleSendQuestion}>
           <textarea
@@ -64,9 +116,9 @@ export function Room() {
           <div className="form-footer">
             {user ? (
               <div className="user-info">
-                  <img src={user.avatar} alt={user.name} />
-                  <span>{user.name}</span>
-                  {/* parei aqui 01:11:17 aula 3 */}
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+                {/* parei aqui 01:11:17 aula 3 */}
               </div>
             ) : (
               <span>
@@ -79,6 +131,8 @@ export function Room() {
             </Button>
           </div>
         </form>
+
+        {JSON.stringify(questions)}
       </main>
     </div>
   );
